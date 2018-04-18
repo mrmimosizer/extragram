307a75f586935eb24a761eb6085d973bc255cf10
mrmimosizer <m1mosizer@gmail.com>
2018-04-18 13:56:41 +1000

    pulled michaels work

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
   has_many :likes
-<<<<<<< HEAD
-  has_many :liked_posts, through: :likes, source: :post 
-=======
   has_many :liked_posts, through: :likes, source: :post
->>>>>>> 34861d09aeed9f6c4eacc51df8064d10036a3591
 end
