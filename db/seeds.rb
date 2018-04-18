34861d09aeed9f6c4eacc51df8064d10036a3591
Michael Bamford <michael@michaelbamford.com>
2018-04-18 13:03:32 +1000

    Add likers to posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-byebug
 if Rails.env.development? then
     User.destroy_all # vs. .delete_all
-    user = User.create! username: 'world_of_engineering', email: 'world_of_engineering@example.com'
+    user = User.create! \
+        username: 'world_of_engineering',
+        email: 'world_of_engineering@example.com',
+        password: 'password',
+        password_confirmation: 'password'
+
     user.posts.create! description: 'world_of_engineering Industry Architecture #engineering'
 end
 
